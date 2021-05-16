a14a6a9945de1dd5242e620f656c8ccee4e51406
Renato Silva de Oliveira <81115873+Oliveira-Renato@users.noreply.github.com>
2021-05-16 02:29:56 -0300

    Update exer1,py

--- ch02/exer1.py
+++ ch02/exer1.py
@@ -1,4 +1,5 @@
-#2. Suppose the cover price of a book is **$24.95, but bookstores get a **40% discount. Shipping costs **$3 for the first copy and **75 cents for each additional copy. What is the total wholesale cost for **60 copies?
+#2. Suppose the cover price of a book is **$24.95, but bookstores get a **40% discount. Shipping costs **$3 for the 
+#  first copy and **75 cents for each additional copy. What is the total wholesale cost for **60 copies?
 
 cover_price = 24.95
 bkstore_dis = cover_price - (cover_price * 40)/100 
